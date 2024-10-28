d37ad0704997a03a1d1a4f4ba6ac12392aa6a800
Mohammad Baker Tarhini <149156843+Mohammad-Tarhini@users.noreply.github.com>
2024-10-28 21:37:49 +0200

    Update diamond.py

--- diamond.py
+++ diamond.py
@@ -1,4 +1,5 @@
 #Write a Python program that prompts the user to enter a number of rows and then generates a symmetrical "palindrome" star pattern, centered to form a diamond shape.
+# IN SOLUTION I take a point  on daimon as orign of yox system and get  an equation of line for each side  
 x=int(input("please enter an positive integer number: "))
 for i in range(-x+1,x):
     for j in range(-x+1,x):
